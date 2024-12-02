fd0d92d4e6ecb45e2db79cc9d187900bc128dc72
“P3rs0n4l.m3. <tuncsoftwarehere@gmail.com>
2024-12-02 20:04:32 +0300

    .

--- socialinnovation/test/app/view/app_test.dart
+++ socialinnovation/test/app/view/app_test.dart
@@ -5,7 +5,7 @@ import 'package:socialinnovation/counter/counter.dart';
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(const AppView());
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
